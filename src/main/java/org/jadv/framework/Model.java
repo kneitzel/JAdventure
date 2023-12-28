8ac18282074669cf3d24e088cdb3e0abddf9188e
Konrad Neitzel <konrad@kneitzel.de>
2023-12-28 20:05:24 +0100

    Added reporting to Maven project including JavaDoc and PMD. Also fixed some JavaDoc issues.

--- src/main/java/org/jadv/framework/Model.java
+++ src/main/java/org/jadv/framework/Model.java
@@ -10,15 +10,15 @@ import java.util.function.Consumer;
  * notifies them automatically of any state changes, usually by calling one of their methods.
  * <p>
  * In this implementation, the Model class maintains a list of consumers that are interested in changes of the model.
- * This list is represented by an instance variable changeListenerList, which is a List of Consumer<Model> objects.
- * Consumer<Model> is a functional interface in Java 8 that takes an object of type Model as input and returns no output.
+ * This list is represented by an instance variable changeListenerList, which is a List of Consumer&lt;Model&gt; objects.
+ * Consumer&lt;Model&gt; is a functional interface in Java 8 that takes an object of type Model as input and returns no output.
  * <p>
  * The Model class provides three methods for interacting with the change listener list:
  * <p>
  * The hasChanged() method notifies all listeners that the model has changed by calling the accept() method on each
- * Consumer<Model> object in the list, passing in this (i.e., the changed model) as an argument.
- * The addChangeListener() method adds a Consumer<Model> object to the list of change listeners.
- * The removeChangeListener() method removes a Consumer<Model> object from the list of change listeners.
+ * Consumer&lt;Model&gt; object in the list, passing in this (i.e., the changed model) as an argument.
+ * The addChangeListener() method adds a Consumer&lt;Model&gt; object to the list of change listeners.
+ * The removeChangeListener() method removes a Consumer&lt;Model&gt; object from the list of change listeners.
  * This implementation is designed to be extended by concrete model classes, which would implement their own methods for
  * changing the state of the model and calling the hasChanged() method when appropriate. View classes that need to be
  * updated whenever the model changes can add themselves as listeners to the model by calling the addChangeListener()
